e5dc79032eff9f7ae8560bfee43efe26960beb23
gzmask <gzmask@gmail.com>
2012-10-22 10:59:33 -0600

    minor

--- src/util.cpp
+++ src/util.cpp
@@ -18,24 +18,33 @@ ImagePanel init_img_panel(ImagePanel img) {
   return img;
 }
 
+//translate ray equation to an 0~255 shading value
 int ray_tracing(Ray ray) {
+  Point p = ray_objects_intersection(ray);
   return ray[0]+ray[1]+ray[2]+ray[3];
 }
 
-//helpers
+//calculate the ray object intersection point
+Point ray_objects_intersection(Ray ray) {
+  return {1,2,3};
+}
 
-//translate 2D array index of row column to 1D index
-//notice that x, or column index, starts with 0
+//==========helpers==========
+
+//Translate 2D array index of row column to 1D index.
+//Notice that x, or column index, starts with 0. 
+//If return value is -1 then there is an out-of-bounce error.
 int to_1d(int x, int y) {
-  if (x >= IMG_X)
+  if (x >= IMG_X || x < 0)
     return -1;
-  if (y >= IMG_Y)
+  if (y >= IMG_Y || y < 0)
     return -1;
   return (IMG_Y*y + x);
 }
 
+//Translate 1d array index to 2d
 std::array<int, 2> to_2d(int x) {
-  if (x>=(IMG_X*IMG_Y)) {
+  if (x>=(IMG_X*IMG_Y) || x < 0) {
     return {-1,-1};
   }
   int y_ = x / IMG_X; 
--- src/util.h
+++ src/util.h
@@ -10,11 +10,13 @@
 #include <functional>
 #include <iostream>
 typedef std::array<int, IMG_LEN> ImagePanel;
+typedef std::array<float, 3> Point;
 typedef std::array<float, 4> Ray;//assuming there are 3 parameters for ray equation
 
 ImagePanel foreach_pixel_exec(ImagePanel, std::function<int(float)>);
 ImagePanel init_img_panel(ImagePanel);
 int ray_tracing(Ray);
+Point ray_objects_intersection(Ray);
 
 //helpers
 int to_1d(int, int);
